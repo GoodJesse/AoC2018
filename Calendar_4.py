89b89fdcc484c2f7457842db5dc18244434599ce
Jesse <jesse.ziegler@a1.net>
2018-12-04 09:59:54 +0100

    Cleanup

--- Calendar_4.py
+++ Calendar_4.py
@@ -6,17 +6,15 @@ data.sort()
 dates = []
 is1021 = []
 for i in range(len(data)):
-    data[i] = data[i][12:]
     if '#' in data[i]:
         if i > 0:
             dates.append(newData)
         newData = [int(data[i][data[i].find('#') + 1: data[i].find('b') - 1])]
 
     else:
-        newData.append(int(data[i][3:5]))
+        newData.append(int(data[i][15:17]))
 
 
-asleep = {}
 times = {}
 for i in range(len(dates)):
     while len(dates[i]) > 2:
@@ -24,16 +22,13 @@ for i in range(len(dates)):
         asl = dates[i].pop()
         if dates[i][0] == 1021:
             is1021.append([asl, aw])
-        if dates[i][0] in asleep:
-            asleep[dates[i][0]] = asleep[dates[i][0]] + aw - asl
-        else:
-            asleep[dates[i][0]] = aw - asl
         if dates[i][0] in times:
             times[dates[i][0]].append([asl, aw])
         else:
             times[dates[i][0]] = [[asl, aw]]
 
 results = {}
+results2 = {}
 for key in times.keys():
 
     hour = [0 for i in range(60)]
@@ -41,13 +36,12 @@ for key in times.keys():
     for i in range(len(times[key])):
         for j in range(times[key][i][0], times[key][i][1]):
             hour[j] += 1
-    number = max(hour)
     minute = hour.index(max(hour))
-    results[number] = [minute, key]
+    results[max(hour)] = [minute, key]
+    results2[sum(hour)] = [minute, key]
 
 solution2 = results[max(results.keys())][0]*results[max(results.keys())][1]
 print solution2
-
-solution1 = hour.index(max(hour))* asleep.keys()[asleep.values().index(max(asleep.values()))]
+solution1 = results2[max(results2.keys())][0]*results2[max(results2.keys())][1]
 print solution1
 
